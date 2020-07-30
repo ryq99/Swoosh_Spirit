94ee213238a75cacfb41c403405548c3552008eb
ryq99 <yrc602@gmail.com>
2020-07-30 16:23:28 -0700

    finished cspdarknet53() backbone.

--- utils/backbone.py
+++ utils/backbone.py
@@ -4,6 +4,7 @@ from utils.block import conv_block, route_group
 def cspdarknet53_tiny(inputs):
     x = conv_block(inputs, channels=32, downsample=True)
     x = conv_block(x, channels=64, downsample=True)
+    #### block start #####
     x = conv_block(x, channels=64)
     route = x
     x = route_group(x, num_groups=2, group_id=1)
@@ -11,11 +12,37 @@ def cspdarknet53_tiny(inputs):
     route_1 = x
     x = conv_block(x, channels=32)
     x = tf.concat([x, route_1], axis=-1)
-    x = conv_block(x, channels=64, kernel_size=1)
+    x = conv_block(x, channels=64, kernel_size=(1, 1))
     x = tf.concat([route, x], axis=-1)
-    x = tf.keras.layers.MaxPool2D(pool_size=2, stride=2, padding='same')(x)
-
+    x = tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='same')(x)
+    #### block start #####
+    x = conv_block(x, channels=128)
+    route = x
+    x = route_group(x, num_groups=2, group_id=1)
+    x = conv_block(x, channels=64)
+    route_1 = x
+    x = conv_block(x, channels=64)
+    x = tf.concat([x, route_1], axis=-1)
+    x = conv_block(x, channels=128, kernel_size=(1, 1))
+    x = tf.concat([route, x], axis=-1)
+    x = tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='same')(x)
+    #### block start #####
+    x = conv_block(x, channels=256)
+    route = x
+    x = route_group(x, num_groups=2, group_id=1)
+    x = conv_block(x, 128)
+    route_1 = x
+    x = conv_block(x, channels=128)
+    x = tf.concat([x, route_1], axis=-1)
+    x = conv_bloack(x, channels=256, kernel_size=(1, 1))
+    x = tf.concat([route, x], axis=-1)
+    x = tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='same')(x)
     
+    x = conv_block(x, channels=512)
+    return route_1, x
+
+
+
 
 
 
